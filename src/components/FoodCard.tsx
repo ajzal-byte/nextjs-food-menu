98a42611363fa16b58861de74c34eedb630c5855
ajzal-byte <ajzalaju10@gmail.com>
2024-09-10 03:12:43 +0530

    placeholder images added. minor ui updates

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,9 +1,17 @@
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 const FoodCard: React.FC<FoodCardProps> = ({ image, title, description }) => (
-  <div className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer">
-    <img src={image} alt={title} className="w-full h-48 object-cover" />
+  <div className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer hover:shadow-2xl">
+    <div className="overflow-hidden">
+      {/* TODO: to later change to next image */}
+      <img
+        src={image}
+        alt={title}
+        className="w-full h-48 object-cover transform transition-transform duration-300 ease-in-out hover:scale-110"
+      />
+    </div>
     <div className="p-4">
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-gray-600 mb-4">{description}</p>
