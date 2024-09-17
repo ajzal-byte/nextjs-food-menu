a033cc49560582730af8730ccdd8af7fdddf279b
ajzal-byte <ajzalaju10@gmail.com>
2024-09-18 04:38:39 +0530

    hydration error fixed

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,9 +1,11 @@
 import { DialogTrigger } from "@radix-ui/react-dialog";
-import { Button } from "./ui/button";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 
+const Button = dynamic(() => import("./ui/button").then((mod) => mod.Button), { ssr: false });
+
 const FoodCard: React.FC<FoodCardProps> = ({ image, title, description }) => (
-  <div className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer hover:shadow-2xl">
+  <div className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer hover:shadow-xl">
     <div className="overflow-hidden">
       {/* TODO: to later change to next image */}
       <img
