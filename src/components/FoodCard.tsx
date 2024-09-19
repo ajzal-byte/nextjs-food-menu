7b94fae38e6e11f83d4f2185255590d54abc59c1
ajzal-byte <ajzalaju10@gmail.com>
2024-09-20 02:34:55 +0530

    ui updations and transitions

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -5,12 +5,14 @@ import Image from "next/image";
 const Button = dynamic(() => import("./ui/button").then((mod) => mod.Button), { ssr: false });
 
 const FoodCard: React.FC<FoodCardProps> = ({ image, title, description }) => (
-  <div className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer hover:shadow-xl">
+  <div className="bg-[#d6e5d8] rounded-lg shadow-lg overflow-hidden cursor-pointer hover:shadow-xl">
     <div className="overflow-hidden">
-      {/* TODO: to later change to next image */}
-      <img
+      <Image
         src={image}
         alt={title}
+        width={0}
+        height={0}
+        unoptimized
         className="w-full h-48 object-cover transform transition-transform duration-300 ease-in-out hover:scale-110"
       />
     </div>
@@ -18,7 +20,7 @@ const FoodCard: React.FC<FoodCardProps> = ({ image, title, description }) => (
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-gray-600 mb-4">{description}</p>
       <DialogTrigger>
-        <Button>Select</Button>
+        {/* <Button className="bg-[#3c443f] hover:bg-[#1e2220]">Select</Button> */}
       </DialogTrigger>
     </div>
   </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Link } from 'next-view-transitions'
 import { UtensilsCrossed } from "lucide-react";
 
 export default function Navbar() {
@@ -12,7 +12,7 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="bg-[#e5e9e3] flex h-20 w-full shrink-0 items-center justify-between px-4 md:px-6">
+    <header className="bg-[#d6e5d8] flex h-20 w-full shrink-0 items-center justify-between px-4 md:px-6">
       <Link href="/" className="mr-6 hidden lg:flex" prefetch={false}>
         <UtensilsCrossed className="h-8 w-8 text-[#343d37]" />
         <span className="sr-only">Next.js Cuisine</span>
@@ -22,9 +22,9 @@ export default function Navbar() {
       <Sheet>
         <SheetTrigger asChild>
           <Button
-            variant="outline"
+            // variant="outline"
             size="icon"
-            className="lg:hidden ml-auto bg-[#e5e9e3] hover:bg-[#C9DABF]"
+            className="lg:hidden text-[#5f685c] hover:text-[#343d37] ml-auto bg-[#d6e5d8] hover:bg-[#C9DABF]"
           >
             <MenuIcon className="h-6 w-6" />
             <span className="sr-only">Toggle navigation menu</span>
@@ -53,7 +53,7 @@ export default function Navbar() {
           <Link
             key={idx}
             href={link.href}
-            className="group inline-flex h-9 w-max items-center justify-center rounded-md text-[#5f685c] hover:text-[#343d37] bg-[#e5e9e3] px-4 py-2 text-sm font-medium transition-colors hover:bg-[#C9DABF] focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md text-[#5f685c] hover:text-[#343d37] bg-[#d6e5d8] hover:bg-[#c1d4cd] px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
             prefetch={false}
           >
             {link.title}
