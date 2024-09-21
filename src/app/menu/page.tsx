361df13fa42842a6213e35d4e815143be0afa7bf
ajzal-byte <ajzalaju10@gmail.com>
2024-09-22 01:58:56 +0530

    got rid of unwanted comp

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,5 +1,16 @@
-import { MenuList } from "@/components";
+import FoodCard from "@/components/FoodCard";
+import { menuItems } from "@/lib/menu";
 
 export default function MenuPage() {
-  return <MenuList />;
+  return (
+    <div className="container mx-auto px-4 py-6 md:py-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-8">
+        {menuItems.map((food) => (
+          <div className="w-full max-w-full sm:max-w-xs mx-auto" key={food.id}>
+            <FoodCard {...food} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
