56d706afbadb08b5e67c85620a55fb8d3673b9a6
ajzal-byte <ajzalaju10@gmail.com>
2024-09-22 05:09:42 +0530

    fixed some ugly ui issues

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -51,29 +51,36 @@ const FoodCard: React.FC<FoodCardProps> = ({
         style={{
           borderRadius: "24px",
         }}
-        className="pointer-events-auto relative flex h-auto w-full flex-col overflow-hidden border border-zinc-950/10 bg-[#d6e5d8] dark:border-zinc-50/10 dark:bg-zinc-900 sm:w-[500px]"
+        className="pointer-events-auto relative flex h-auto w-[calc(100%-2rem)] max-w-[500px] max-h-[80vh] flex-col overflow-hidden border border-zinc-950/10 bg-[#d6e5d8] dark:border-zinc-50/10 dark:bg-zinc-900 mx-auto"
       >
-        <DialogImage src={image} alt={title} className="h-full w-full" />
-        <div className="p-6">
-          <DialogTitle className="text-2xl text-zinc-950 dark:text-zinc-50 font-semibold">
-            {title}
-          </DialogTitle>
-          <DialogSubtitle className="text-zinc-700 dark:text-zinc-400">
-            {category}
-          </DialogSubtitle>
-          <DialogDescription
-            disableLayoutAnimation
-            variants={{
-              initial: { opacity: 0, scale: 0.8, y: 100 },
-              animate: { opacity: 1, scale: 1, y: 0 },
-              exit: { opacity: 0, scale: 0.8, y: 100 },
-            }}
-          >
-            <p className="mt-2 text-zinc-500 dark:text-zinc-500">
-              {description}
-            </p>
-            {/* <p className="text-zinc-500">{description}</p> */}
-          </DialogDescription>
+        <div className="relative w-full pt-[56.25%]">
+          <DialogImage
+            src={image}
+            alt={title}
+            className="absolute top-0 left-0 w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex-grow overflow-y-auto scrollbar-thin scrollbar-thumb-zinc-400 scrollbar-track-transparent hover:scrollbar-thumb-zinc-500">
+          <div className="p-6">
+            <DialogTitle className="text-2xl text-zinc-950 dark:text-zinc-50 font-semibold">
+              {title}
+            </DialogTitle>
+            <DialogSubtitle className="text-zinc-700 dark:text-zinc-400">
+              {category}
+            </DialogSubtitle>
+            <DialogDescription
+              disableLayoutAnimation
+              variants={{
+                initial: { opacity: 0, scale: 0.8, y: 100 },
+                animate: { opacity: 1, scale: 1, y: 0 },
+                exit: { opacity: 0, scale: 0.8, y: 100 },
+              }}
+            >
+              <p className="mt-2 text-zinc-500 dark:text-zinc-500">
+                {description}
+              </p>
+            </DialogDescription>
+          </div>
         </div>
         <DialogClose className="text-zinc-50" />
       </DialogContent>
